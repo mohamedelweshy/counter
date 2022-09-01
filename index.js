cb909639c175a9aa43b46d10b22a1392bb750937
mohamed elweshy <mohamedelweshy122@gmail.com>
2022-09-01 17:55:24 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function(){
+    count-=1;
+    document.getElementById("countLabel").innerHTML=count;
+
+}
+
+document.getElementById("resetBtn").ononclick = function(){
+    count=0;
+    document.getElementById("countLabel").innerHTML=count;
+
+}
+
+document.getElementById("increaseBtn").onclick = function(){
+    count+=1;
+    document.getElementById("countLabel").innerHTML=count;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
